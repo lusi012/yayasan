d41866502e925b23ce523f4aebd70b90566bd0b3
lusi012 <kristianalusi991@gmail.com>
2025-04-16 20:19:40 +0700

    p

--- resources/views/pengguna_umum/informasi.blade.php
+++ resources/views/pengguna_umum/informasi.blade.php
@@ -7,7 +7,7 @@
     <title>Information - Yayasan Cahaya Ayu Kota Pontianak</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet" />
-    <link rel="stylesheet" href="{{ asset('yayasan/style.css')}}">
+    <link rel="stylesheet" href="{{ asset('yayasan/style.css') }}">
 </head>
 
 <body>
@@ -18,35 +18,52 @@
             @yield('content')
         </div>
     </header>
-    
-    <main class="container mx-auto py-8">
 
-        <h1 class="text-4xl font-bold mb-6 text-center">VISI : </h1>
-        <p class="text-lg mb-4">
-            Yayasan cahaya Ayu merupakan yayasan yang menyediakan pelatihan kerja swasta
-            bagi kebutuhan masyarakat yang putus sekolah dan ingin bekerja.
-        </p>
-        <h2 class="text-2xl font-bold mb-4">MISI : </h2>
-        <p class="text-lg mb-4">
-            Misi dari Yayasan Cahaya Ayu ini adalah menyediakan tenaga kerja yang handal dan
-            berkualitas sehingga menjadikan tenaga kerja profesional
-        </p>
-        <h2 class="text-2xl font-bold mb-4">MENYEDIAKAN LAYANAN : </h2>
-        <ul class="list-disc list-inside mb-4">
-            <li>Baby sitter</li>
-            <li>Perawat Lansia</li>
-            <li>ART (Asisten Rumah Tangga)</li>
-        </ul>
-    </main>
+    <main>
+        <section class="sec1">
+            <article class="artsec1">
+                <h1>Tentang Perusahaan</h1>
+                <p>LPKS Cahaya Ayu merupakan sebuah instansi pemerintah, badan hukum atau perorangan yang memenuhi persyaratan untuk menyelenggarakan Pelatihan Kerja</p>
+            </article>
+            <article>
+                <img src="https://via.placeholder.com/80" alt="">
+            </article>
+        </section>
+
+        <section class="visimisi-container">
+          <article class="box">
+            <h2>Visi</h2>
+            <div class="content">
+              <img src="https://via.placeholder.com/80" alt="Logo">
+              <p>
+                Yayasan Cahaya Ayu merupakan yayasan yang menyediakan pelatihan kerja swasta
+                bagi kebutuhan masyarakat yang putus sekolah dan ingin bekerja.
+              </p>
+            </div>
+          </article>
+
+          <article class="box">
+            <h2>Misi</h2>
+            <div class="content">
+              <img src="https://via.placeholder.com/80" alt="Logo">
+              <p>
+                Misi dari Yayasan Cahaya Ayu ini adalah menyediakan tenaga kerja yang handal dan
+                berkualitas sehingga menjadikan tenaga kerja profesional.
+              </p>
+            </div>
+          </article>
+        </section>
+      </main>
+
     <footer>
         <p>&copy; 2023 Yayasan Cahaya Ayu Kota Pontianak</p>
         <ul class="social-footer">
-          <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-          <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-          <li><a href="#"><i class="fab fa-tiktok"></i></a></li>
-          <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+            <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+            <li><a href="#"><i class="fab fa-tiktok"></i></a></li>
+            <li><a href="#"><i class="fab fa-youtube"></i></a></li>
         </ul>
-      </footer>
+    </footer>
 </body>
 
 </html>
