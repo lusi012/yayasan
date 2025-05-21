f83dac159d11dfadfa6fc27293bd2135e4672874
canelyne <greciavanysamallo@gmail.com>
2025-05-21 21:11:10 +0700

    p

--- resources/views/admin/layout/main.blade.php
+++ resources/views/admin/layout/main.blade.php
@@ -133,7 +133,6 @@
             </div>
         </div>
     </div>
-    {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script> --}}
 
     <script src="{{ asset('tmp_admin/dist/modules/jquery.min.js') }}"></script>
     <script src="{{ asset('tmp_admin/dist/modules/popper.js') }}"></script>
