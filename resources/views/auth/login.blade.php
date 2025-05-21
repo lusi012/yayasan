ea9117fb1fc7a30310a3008c38fda9048e7ec10f
lusi012 <kristianalusi991@gmail.com>
2025-05-21 20:17:43 +0700

    Update lagi

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,9 +10,41 @@
 </head>
 
 <body>
-    @include('sweetalert::alert')
 
+    @include('sweetalert::alert')
     <div class="wrapper">
+        <form action="{{ route('admin.loginproses') }}" method="POST">
+            @csrf
+
+            <div class="avatar">
+                {{-- <img src="{{ asset('avatar.png') }}" alt="Avatar"> --}}
+                <img src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar">
+            </div>
+
+            <h2>LOGIN ADMIN</h2>
+
+            <div class="input-field">
+                <label>Username</label>
+                <input type="text" name="username" required>
+
+            </div>
+            <div class="input-field">
+                <label>Password</label>
+                <input type="password" name="password" required>
+
+            </div>
+
+            <div class="options">
+                <label><input type="checkbox" name="remember"> Remember Me</label>
+                <a href="{{ route('auth.lupapassword') }}">Ubah Password</a>
+            </div>
+
+            <button type="submit" class="login-btn">Login</button>
+        </form>
+    </div>
+
+
+    {{-- <div class="wrapper">
         <form action="{{ route('admin.loginproses') }}" method="POST">
             @csrf
             <h1>YAYASAN CAHAYA AYU</h1>
@@ -31,7 +63,7 @@
             </div>
             <button type="submit">Log In</button>
         </form>
-    </div>
+    </div> --}}
 </body>
 
 </html>
